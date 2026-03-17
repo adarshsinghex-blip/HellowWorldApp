78cf093adc1ff3a84f91061dbe3900140910421c
adarshsinghex-blip <adarshsinghex@gmail.com>
2026-03-17 09:15:35 +0530

    Add files via upload

---
+++ HelloWorldApp.java
@@ -0,0 +1,14 @@
+class HelloWorldApp {
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("      Java UC1 Console Demo      ");
+        System.out.println("=================================");
+
+        System.out.println("Hello World!");
+
+        System.out.println("---------------------------------");
+        System.out.println("Program executed successfully.");
+        System.out.println("Welcome to the OOPS Banner App.");
+    }
+}
