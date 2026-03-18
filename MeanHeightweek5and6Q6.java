901ebb5ca7314cc7c6547821ae0d5da8793214d2
adarshsinghex-blip <adarshsinghex@gmail.com>
2026-03-18 10:52:09 +0530

    Add files via upload

---
+++ MeanHeightweek5and6Q6.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class MeanHeight {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double[] heights = new double[11];
+        double sum = 0;
+
+        System.out.println("🏃 Enter heights of 11 football players (in cm):");
+
+        for (int i = 0; i < 11; i++) {
+            System.out.print("Player " + (i + 1) + ": ");
+            heights[i] = sc.nextDouble();
+            sum += heights[i];
+        }
+
+        double mean = sum / 11;
+
+        System.out.println("\nAverage height of the team is: " + mean + " cm");
+
+        sc.close();
+    }
+}
